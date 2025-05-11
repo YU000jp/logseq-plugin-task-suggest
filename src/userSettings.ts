805b36d1aea45845fdacc8966a2318ed139adff5
YU <111847207+YU000jp@users.noreply.github.com>
2025-05-12 08:38:20 +0900

    sort userSettings

--- src/userSettings.ts
+++ src/userSettings.ts
@@ -2,18 +2,22 @@ import { t } from "logseq-l10n"
 
 export const userSettings = () => logseq.useSettingsSchema([
 
+  {// 編集モードで検出するマーカーの種類
+    key: "triggerMarker",
+    title: t("Types of markers to be detected in edit mode"),
+    type: "string",
+    default: "TODO",
+    description: `
+      For MD graph (both DB version):
+      ${t("Choose from \`TODO, LATER, NOW, DOING, WAITING, DONE\`, separated by a blank space.")}
+      `,
+  },
+
   {// タスク以外のブロックも含めるかどうか
     key: "includeNonTaskBlocks",
     title: t("Include non-task blocks"),
     type: "boolean",
     default: false,
-    description: t("Enable"),
-  },
-  {// breadcrumbsを有効にするかどうか
-    key: "enableBreadcrumb",
-    title: t("Enable breadcrumbs"),
-    type: "boolean",
-    default: true,
     description: t("Enable"),
   },
   {// 検索に含めるマーカーの種類
@@ -26,15 +30,13 @@ export const userSettings = () => logseq.useSettingsSchema([
       ${t("Choose from \`TODO, LATER, NOW, DOING, WAITING, DONE\`, separated by a blank space.")}
       `,
   },
-  {// 編集モードで検出するマーカーの種類
-    key: "triggerMarker",
-    title: t("Types of markers to be detected in edit mode"),
-    type: "string",
-    default: "TODO",
-    description: `
-      For MD graph (both DB version):
-      ${t("Choose from \`TODO, LATER, NOW, DOING, WAITING, DONE\`, separated by a blank space.")}
-      `,
+
+  {// breadcrumbsを有効にするかどうか
+    key: "enableBreadcrumb",
+    title: t("Enable breadcrumbs"),
+    type: "boolean",
+    default: true,
+    description: t("Enable"),
   },
   {// breadcrumbsから除外する単語のリスト（改行区切り）
     key: "tocRemoveWordList",
